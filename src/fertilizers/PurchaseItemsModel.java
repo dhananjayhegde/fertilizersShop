d9d383a1b0f5f671df6bcb52b4f58570f6297caf
Dhananjay Hegde <dananjay123.gmail.com>
2016-04-03 08:08:51 +0530

    PurchaseItemModel is built now!

--- src/fertilizers/PurchaseItemsModel.java
+++ src/fertilizers/PurchaseItemsModel.java
@@ -11,4 +11,70 @@ package fertilizers;
  */
 public class PurchaseItemsModel {
     
+    private long id;
+    private int itemNo;
+    private long productId;
+    private double price;
+    private int quantity;
+    private double amount;
+
+    public PurchaseItemsModel(long id, int itemNo, long productId, double price, int quantity, double amount) {
+        this.id = id;
+        this.itemNo = itemNo;
+        this.productId = productId;
+        this.price = price;
+        this.quantity = quantity;
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public int getItemNo() {
+        return itemNo;
+    }
+
+    public void setItemNo(int itemNo) {
+        this.itemNo = itemNo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
 }
